31d3e694abc648796556a5b1c1fe3b62487d4615
zamhaq <haque864@gmail.com>
2018-06-26 13:21:52 +0530

    Add method to edit a todo

---
+++ src/components/Todos.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import update from 'immutability-helper';
+
+class Todos extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+      text: ""
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    const todo = this.state.text;
+    this.setState({ todos: [...this.state.todos, todo] });
+  }
+  handleChange(e) {
+    this.setState({ text: e.target.value });
+  }
+  handleTodoChange(e, i) {
+    console.log(e.target.value, i);
+    const text = e.target.value;
+    const newTodos = update(this.state.todos, {[i]: {$set: text}});
+    this.setState({todos: newTodos});
+  }
+  render() {
+    const { todos } = this.state;
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            value={this.state.text}
+            onChange={this.handleChange}
+            name="text"
+            type="text"
+          />
+          <input type="submit" value="submit" />
+        </form>
+        <ul>
+          {todos.map((todo, i) => (
+            <li key={i}>
+              <input
+                type="text"
+                name={todo}
+                value={todo}
+                onChange={e => {
+                  this.handleTodoChange(e, i);
+                }}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Todos;
